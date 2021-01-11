c9e501a1955165d08c07767e6e55c53cc3707120
MedinaVilla <net_medina@hotmail.com>
2021-01-11 17:55:58 -0600

    a

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React, { Suspense, lazy } from "react";
 import './App.scss';
 import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import PageLoading from './components/layout/PageLoading';
-
+import Navbar from './components/layout/NavBar';
 
 const MainPage = lazy(() => import('./components/MainPage'));
 const Login = lazy(() => import('./components/acceso/login/Login'));
 const Registro = lazy(() => import('./components/acceso/registro/Registro'));
 const RegistroArbol = lazy(() => import('./components/arboles/RegistroArbol'));
-const Navbar = lazy(() => import('./components/layout/NavBar'));
 const Footer = lazy(() => import('./components/layout/Footer'));
 const NotFound = lazy(() => import('./components/layout/404'));
-const MapaArbolado  = lazy(() => import('./components/mapa/MapaArbolado'));
-const SolicitudServicio  = lazy(() => import('./components/servicio/nuevo/Servicio'));
+const MapaArbolado = lazy(() => import('./components/mapa/MapaArbolado'));
+const SolicitudServicio = lazy(() => import('./components/servicio/nuevo/Servicio'));
 const RepresentanteAlcadia = lazy(() => import('./components/admin/RegistroRepresentanteAlcaldia'));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Suspense fallback={<PageLoading />}>
-          <Navbar />
           <Switch>
             <Route exact path="/" component={MainPage} />
             <Route exact path="/login" component={Login} />
@@ -33,7 +32,7 @@ function App() {
             <Route exact path="/404" component={NotFound} />
             <Redirect to="/404" />
           </Switch>
-          <Footer/>
+          <Footer />
         </Suspense>
       </BrowserRouter>
     </div>
